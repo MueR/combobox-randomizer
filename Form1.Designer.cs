7632192d16d18505a6d748fd9441a2ea207890b1
MueR <bas@muer.nl>
2021-11-30 22:04:26 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace JulianComboboxRandomizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.BtnRandomize = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Min value:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Max value:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(139, 23);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // BtnRandomize
+            // 
+            this.BtnRandomize.Location = new System.Drawing.Point(12, 52);
+            this.BtnRandomize.Name = "BtnRandomize";
+            this.BtnRandomize.Size = new System.Drawing.Size(248, 40);
+            this.BtnRandomize.TabIndex = 4;
+            this.BtnRandomize.Text = "Randomize";
+            this.BtnRandomize.UseVisualStyleBackColor = true;
+            this.BtnRandomize.Click += new System.EventHandler(this.BtnRandomize_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ResultLabel.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ResultLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ResultLabel.Location = new System.Drawing.Point(13, 99);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(247, 40);
+            this.ResultLabel.TabIndex = 5;
+            this.ResultLabel.Text = "24";
+            this.ResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 155);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.BtnRandomize);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button BtnRandomize;
+        private System.Windows.Forms.Label ResultLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace JulianComboboxRandomizer
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i < 10; i++)
+            {
+                this.comboBox1.Items.Add(i);
+            }
+            for (int i = 90; i < 99; i++)
+            {
+                this.comboBox2.Items.Add(i);
+            }
+        }
+
+        private void BtnRandomize_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.comboBox1.SelectedItem == null || this.comboBox2.SelectedItem == null)
+                {
+                    this.ResultLabel.Text = "Empty arg";
+                    return;
+                }
+                int min = Int32.Parse(this.comboBox1.SelectedItem.ToString());
+                int max = Int32.Parse(this.comboBox2.SelectedItem.ToString());
+                this.ResultLabel.Text = new Random().Next(min, max).ToString();
+            }
+            catch (ArgumentNullException)
+            {
+                this.ResultLabel.Text = "Empty arg";
+            }
+            catch (FormatException)
+            {
+                this.ResultLabel.Text = "Invalid arg";
+            }
+            catch (OverflowException)
+            {
+                this.ResultLabel.Text = "Invalid arg";
+            }
+        }
+    }
+}
